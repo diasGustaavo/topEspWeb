54c9a442bc79c00f3dc29f1ba2308f9f8c9566dc
diasGustaavo <diasgustaavo@icloud.com>
2023-04-18 10:08:58 -0300

    implement converteMoeda using the economia.awesomeapi

--- Zolde/js/converteMoeda.js
+++ Zolde/js/converteMoeda.js
@@ -1,18 +1,47 @@
-var numero = 9.9;
-var formatado = numero.toFixed(2);
-formatado = "R$ " + formatado;
-formatado = formatado.replace(".", ",");
-console.log(formatado);
-var texto = "R$ 120,35";
-var soNumero = texto.replace("R$ ", "");
-soNumero = soNumero.replace(",", ".");
-var valor = parseFloat(soNumero);
-console.log(valor);
+let precoDolar = 0;
+let precoReal = 0;
 
-numero = 9.9;
-formatado = "R$ " + numero.toFixed(2).replace(".", ",");
-console.log(formatado);
+fetch('https://economia.awesomeapi.com.br/last/USD-BRL')
+  .then(response => response.json())
+  .then(data => {
+    precoDolar = parseFloat(data["USDBRL"]["ask"]);
+    precoReal = 1 / precoDolar
+  })
+  .catch(error => {
+    console.error('Error fetching API:', error);
+  });
 
-texto = "R$ 120,35";
-valor = parseFloat(texto.replace("R$ ", "").replace(",", "."));
-console.log(valor);
+// Get the button element by its ID
+const button = document.getElementById('moedaButton');
+
+let brlPrice = true
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+    console.log("button clicked!")
+    brlPrice = !brlPrice
+    
+    // Get all the figcaption elements
+    let figcaptions = document.getElementsByTagName("figcaption");
+
+    // Loop through each figcaption and cut off text after "$" sign
+    for (let i = 0; i < figcaptions.length; i++) {
+        let figcaption = figcaptions[i];
+        let index = figcaption.textContent.indexOf("$");
+
+        if (index > -1) {
+            let contentAfterDollar = parseFloat(figcaption.textContent.substring(index + 1));
+
+            if (brlPrice) {
+                console.log("entrou aqui talkey")
+                figcaption.textContent = figcaption.textContent.substring(0, index + 1) + parseInt(contentAfterDollar * precoReal);
+                figcaption.textContent = figcaption.textContent.replace(/R\$/g, "$");
+
+            } else {
+                console.log("entrou aqui companheiro")
+                figcaption.textContent = figcaption.textContent.substring(0, index + 1) + parseInt(contentAfterDollar * precoDolar);
+                figcaption.textContent = figcaption.textContent.replace(/\$/g, "R$");
+            }
+            console.log("Content after $: " + contentAfterDollar);
+        }
+    }
+});
--- Zolde/js/home.js
+++ Zolde/js/home.js
@@ -33,3 +33,43 @@ $('.mais-vendidos button').click(function() {
   $('.mais-vendidos').removeClass('painel-compacto');
 });
 
+// Define the product list with their corresponding prices
+var cars1 = [
+	{name: "2003 Chevrolet SSR", price: 59900},
+	{name: "1973 Volkswagen Thing", price: 49900},
+	{name: "1974 Vanguard-Sebring CitiCar", price: 39900},
+	{name: "1973 Reliant Robin", price: 199900},
+	{name: "2013 Pontiac Aztek", price: 13900},
+	{name: "2011 Nissan Murano CrossCabriolet", price: 10900},
+	{name: "1961 Amphicar Model 770", price: 79900},
+	{name: "1935 Bugatti Type 57SC Atlantic", price: 40500000}
+];
+
+const cars2 = [
+	{name: "1962 Invader GT", price: 129900},
+	{name: "1981 Gurgel X-15", price: 13900},
+	{name: "1991 Gurgel BR-800", price: 18900},
+	{name: "1975 Gurgel Itaipu", price: 25900},
+	{name: "1990 Gurgel Patrol", price: 599900},
+	{name: "1956 BMW Isetta", price: 129900},
+	{name: "2003 Chevrolet SSR", price: 59900},
+	{name: "1973 Volkswagen Thing", price: 49900}
+];  
+
+// Get all the product elements
+var novidades = $('.painel.novidades li');
+
+// Update the prices for each product
+novidades.each(function(index) {
+	var product = cars1[index];
+	$(this).find('figcaption').text(product.name + " por $" + product.price);
+});
+
+// painel mais-vendidos
+var maisVendidos = $('.painel.mais-vendidos li');
+
+// Update the prices for each product
+maisVendidos.each(function(index) {
+	var product = cars1[index];
+	$(this).find('figcaption').text(product.name + " por $" + product.price);
+});
